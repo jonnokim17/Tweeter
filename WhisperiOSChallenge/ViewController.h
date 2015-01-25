6df96f9ff2507329dc0bd6bee8576a43e5a25f6e
Jonathan Kim <jonnokim17@gmail.com>
2015-01-25 14:26:54 -0800

    Initial Commit

---
+++ WhisperiOSChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhisperiOSChallenge
+//
+//  Created by Jonathan Kim on 1/25/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WhisperiOSChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhisperiOSChallenge
+//
+//  Created by Jonathan Kim on 1/25/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
